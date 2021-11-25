f94fa4097f3c0f07f857fb5c9fbad1480416e7a6
Tyler Smith <tylerlwsmith@gmail.com>
2021-11-25 11:18:35 -0800

    Scroll accessibility improvements

--- components/ScrollLink.tsx
+++ components/ScrollLink.tsx
@@ -1,12 +1,27 @@
+// @ts-nocheck
 export default function ScrollLink({ href, ...props }) {
   function scrollToSection(
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
   ) {
     event.preventDefault();
-    // @ts-ignore
+
     const sectionId = new URL(event.target.href).hash;
     const section = document.querySelector(sectionId);
-    section.scrollIntoView();
+    if (!section) return;
+
+    const hasTabIndex = section.hasAttribute("tabindex");
+    const hasOutline = section.style.outline ? true : false;
+
+    !hasTabIndex && section.setAttribute("tabindex", "-1");
+    !hasOutline && (section.style.outline = "none");
+
+    section.focus();
+
+    section.addEventListener("blur", function blurListener() {
+      section.removeEventListener("blur", blurListener);
+      !hasTabIndex && section.removeAttribute("tabindex");
+      !hasOutline && section.style.removeProperty("outline");
+    });
   }
   return <a {...props} href={href} onClick={scrollToSection} />;
 }
