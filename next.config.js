b151b1bc59fdbbdd2ea45fa91f738952367ca7d7
Tyler Smith <tylerlwsmith@gmail.com>
2022-01-02 23:05:09 -0800

    Remove locales

--- next.config.js
+++ next.config.js
@@ -6,10 +6,6 @@ const withMDX = require("@next/mdx")({
 const nextConfig = {
   /** Tell Next that it should render MDX files as page if they're in the page directory. */
   pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
-  i18n: {
-    locales: ["en-US"],
-    defaultLocale: "en-US",
-  },
 };
 
 /** Wrap the config so it can add all of the necessary configuration to process MDX iles. */
